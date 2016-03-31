fdbb3a873f977e3bbadd5979de353661b66a87c5
Connor <nailboyz0247@cox.net>
2016-03-30 17:22:49 -0700

    Initial Commit

---
+++ Assets/Scripts/CrunchyControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrunchyControl : MonoBehaviour {
+
+	private Rigidbody2D myRigidBody;
+	public float bunnyJumpForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonUp ("Jump")) {
+			myRigidBody.AddForce (transform.up * bunnyJumpForce);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			
+		}
+	}
+}

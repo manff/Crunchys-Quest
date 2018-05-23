bdcad993d1a3b4a658d98385602801a2f669d6ad
Connor <nailboyz0247@cox.net>
2018-05-23 13:03:19 -0700

    Hopefully Working

--- Assets/Scripts/CanvasControl.cs
+++ Assets/Scripts/CanvasControl.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class CanvasControl : MonoBehaviour {
 
@@ -8,14 +9,43 @@ public class CanvasControl : MonoBehaviour {
 	CrunchyControl crunchyControl;
 	public Text coinsText;
 
+	public GameObject PausePanel;
+	public bool paused;
+
 	// Use this for initialization
 	void Start () {
 		crunchy = GameObject.Find ("Crunchy");
 		crunchyControl = crunchy.GetComponent<CrunchyControl> ();
+
+		PausePanel.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		coinsText.text = (crunchyControl.coins).ToString("Coins: 0");
+
+		if (paused) {
+			PausePanel.SetActive (true);
+			Time.timeScale = 0;
+		} else {
+			PausePanel.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void PauseMenuOn () {
+		paused = true;
+	}
+
+	public void PauseMenuOff () {
+		paused = false;
+	}
+
+	public void LevelSelect () {
+		SceneManager.LoadScene ("LevelSelect");
+	}
+
+	public void MainMenu () {
+		SceneManager.LoadScene ("Title");
 	}
 }
